0eb0eca5597b4431aea85ac32d34d7d4dee4a17d
Chris Kruining <chris@kruining.eu>
2018-03-28 21:17:02 +0200

    going to test sw live

--- js/engine/game.js
+++ js/engine/game.js
@@ -6,8 +6,8 @@ export default class Game
 {
     constructor()
     {
-        // navigator.serviceWorker.register('./worker.js')
-        //     .then(r => console.log(e), e => console.error(e));
+        navigator.serviceWorker.register('./worker.js')
+            .then(r => console.log(e), e => console.error(e));
         
         new Scene();
     }
