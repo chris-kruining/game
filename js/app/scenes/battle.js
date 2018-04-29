1341ecfad7312dc080713be53dc228ae9e3e97da
Chris Kruining <chris@kruining.eu>
2018-04-29 17:50:48 +0200

    added some of the new assets to the scene

--- js/app/scenes/battle.js
+++ js/app/scenes/battle.js
@@ -16,11 +16,15 @@ export default class Battle extends Rasher.Scene {
     setup(game)
     {
         this.add(new Rasher.Background(this.renderer, 'background'));
+        this.add(new Rasher.Background(this.renderer, 'background_overlay'));
+        this.add(new Rasher.Background(this.renderer, 'attack')).then(a => a.blink(500));
         
         for(let [i, [x, y, s, w, h, r, n, a, b]] of Object.entries(enemies))
         {
             this.add(new Digimon(this, n, new Calculus.Vector4(x, y, w, h), s, Number.parseInt(i), a, b));
         }
+        
+        this.add(new Rasher.Background(this.renderer, 'foreground'));
     }
     
     loop(game)
@@ -32,6 +36,9 @@ export default class Battle extends Rasher.Scene {
         return {
             resources: Object.assign({
                 background: 'img/digimon/scenes/backgrounds/accessglacier.png',
+                background_overlay: 'img/digimon/scenes/backgrounds/battle-overlay.png',
+                attack: 'img/digimon/scenes/backgrounds/attack-2.png',
+                foreground: 'img/digimon/scenes/foregrounds/battle.png',
                 music: 'audio/digimon/dawn/battle_02.mp3',
             }, enemies.reduce((t, e) => {
                 t[e[6]] = `img/digimon/${e[5]}/${e[6]}.png`;
